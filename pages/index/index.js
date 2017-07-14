30e9269b0c9d59013659fb163edee683ed635812
liuzhenrong <liu.zhenrong@jyall.com>
2017-07-14 15:44:02 +0800

    demo刚建立

---
+++ pages/index/index.js
@@ -0,0 +1,87 @@
+// find.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movies: [
+      'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
+      'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',
+      'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg',
+      'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg',
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    logos: [{
+      image: "/image/grzx_kf.png",
+      title: "菜单一"
+    }, {
+        image: "/image/grzx_kf.png",
+      title: "菜单二"
+    }, {
+        image: "/image/grzx_kf.png",
+      title: "菜单三"
+    }, {
+        image: "/image/grzx_kf.png",
+      title: "菜单四"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
